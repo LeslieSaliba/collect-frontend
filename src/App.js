73355a582bfbe8f9cd538ccf8ab57612a38530dc
Leslie Saliba <lesliesaliba@gmail.com>
2023-12-03 18:41:21 +0200

    Feat admin sidebar no routing

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import HomePage from './Pages/HomePage';
 import ContactUs from './Pages/ContactUs';
 import AboutUs from './Pages/AboutUs';
 import Cart from "./Pages/Cart";
+import AdminDashboard from './Pages/AdminDashboard';
+import SidebarAdmin from './DashComponents/sidebarAdmin';
 
 function App() {
   return (
     <div className="App">
-       {/* <Wishlist/> */}
+      {/* <Wishlist/> */}
       {/* <HomePage/> */}
-       {/* <ContactUs /> */}
+      {/* <ContactUs /> */}
       {/* <Cart /> */}
-      <AboutUs/>
+      {/* <AboutUs /> */}
+      {/* <AdminDashboard /> */}
+      <SidebarAdmin />
     </div>
   );
 }
---
+++ src/Pages/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import SidebarAdmin from '../DashComponents/sidebarAdmin';
+
+function AdminDashboard() {
+
+    return (
+        <div>
+            <SidebarAdmin />
+        </div>
+    );
+}
+
+export default AdminDashboard;
