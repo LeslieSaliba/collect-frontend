def921542bf35acea32e6fd59a3bbd878c6d8536
AyaMouawi <ayamouawi8@gmail.com>
2023-12-16 00:45:31 +0200

    responsive about-contact-nav and add edit address

--- src/AboutUsComponents/AboutUsHeader.jsx
+++ src/AboutUsComponents/AboutUsHeader.jsx
@@ -2,23 +2,22 @@ import "../css/AboutUsHeader.css";
 function AboutUsHeader() {
   return (
     <div className="aboutusHeader-cont pt-12">
-    <div className="max-w-screen flex mx-40 gap-40 p-4 ">
-      <div className="w-1/2">
-        <p href="" className="text-4xl italic  mb-24 font-medium">
+       <p href="" className="text-4xl italic  mb-24 font-medium ml-40 pl-4 aboutusHeader-title">
           About Collect
         </p>
-        <p className="text-2xl">
+    <div className="max-w-screen flex mx-40 gap-40 p-4 aboutusHeader-mini">
+      <div className="w-1/2 aboutusHeader-title-cont">
+        <p className="text-2xl aboutusHeader-description">
           Small description about Collect concept and team. Small description
           about Collect concept and team. Small description about Collect
           concept and team. Small description about Collect concept and team.{" "}
         </p>
       </div>
-
-      <div className="w-1/2 ml-32 mt-32 ">
+      <div className="w-1/2 ml-32 mt-32 aboutusHeader-img-cont">
         <img
           src="../Images/boat.png"
           alt="Hero Image"
-          className=""
+          className="aboutusHeader-img"
         />
       </div>
     </div>
