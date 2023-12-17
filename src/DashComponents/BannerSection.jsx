2fa2ac27042901f778477364c424280d9026a417
Leslie Saliba <lesliesaliba@gmail.com>
2023-12-17 12:17:50 +0200

    Feat set and fetch highlighted banner

--- src/DashComponents/BannerSection.jsx
+++ src/DashComponents/BannerSection.jsx
@@ -1,31 +1,73 @@
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import "../css/Dashboard.css";
-import EditBanner from './DashModals/EditBanner';
+import EditBanner from "./DashModals/EditBanner";
 
 function BannerSection() {
     const [banners, setBanners] = useState([]);
     const [showEditBannerModal, setShowEditBannerModal] = useState(false);
     const [selectedBannerID, setSelectedBannerID] = useState(null);
     const [selectedBanner, setSelectedBanner] = useState(null);
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
 
     const fetchBanners = () => {
-        axios.get(`${process.env.REACT_APP_API_URL}/banner/getAll`)
+        axios
+            .get(`${process.env.REACT_APP_API_URL}/banner/getAll`)
             .then((response) => {
                 setBanners(response.data.data);
             })
             .catch((error) => {
                 console.error(`Error fetching banners' data: `, error);
             });
-    }
+    };
+    
+    const changeHighlited = async (e, bannerID) => {
+        let updatedIsClicked = e.target.value;
+        if (updatedIsClicked) {
+            updatedIsClicked = true;
+        }
+        try {
+            banners.map(async (banner) => {
+                if (banner._id !== bannerID) {
+                    console.log("banner._id", banner._id);
+                    const response = await axios.put(
+                        `${process.env.REACT_APP_API_URL}/banner/updateHiglited/${banner._id}`,
+                        { higlighted: false },
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${token}`,
+                            },
+                        }
+                    );
+                    console.log("Response after update request1:", banner._id, response);
+                } else {
+                    const response = await axios.put(
+                        `${process.env.REACT_APP_API_URL}/banner/updateHiglited/${bannerID}`,
+                        { higlighted: updatedIsClicked },
+                        {
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: `Bearer ${token}`,
+                            },
+                        }
+                    );
+                    console.log("Response after update request2:", banner._id, response);
+                }
+            });
+
+            fetchBanners()
+        } catch (error) {
+            console.log("Response after update request:", error.message);
+        }
+    };
 
     useEffect(() => {
         fetchBanners();
     }, []);
 
     const openEditBannerModal = (bannerID) => {
-        const selected = banners.find(banner => banner._id === bannerID);
+        const selected = banners.find((banner) => banner._id === bannerID);
         setSelectedBanner(selected);
         setSelectedBannerID(bannerID);
         setShowEditBannerModal(true);
@@ -37,7 +79,6 @@ function BannerSection() {
 
     return (
         <div>
-
             <div className="overflow-x-auto">
                 <table className="min-w-full divide-y divide-gray-200">
                     <thead>
@@ -52,18 +93,31 @@ function BannerSection() {
                     </thead>
                     <tbody>
                         {banners.map((banner) => (
-                            <tr key={banner._id} className='border-b'>
+                            <tr key={banner._id} className="border-b">
                                 <td className="px-4 py-2">{banner.text}</td>
                                 <td className="px-4 py-2 uppercase">{banner.textButton}</td>
                                 <td className="px-4 py-2 w-1/4">{banner.link}</td>
-                                <td className="px-4 py-2"><img src={banner.image} alt="" /></td>
-                                <td className="px-4 py-2"><input type="checkbox" /></td>
                                 <td className="px-4 py-2">
-                                    <img className='h-6 w-6' src="../Images/dashboardIcons/edit.png" alt="edit"
-                                        onClick={() => openEditBannerModal(banner._id)} />
+                                    <img src={banner.image} alt="" />
+                                </td>
+                                <td className="px-4 py-2">
+                                    <input
+                                        type="radio"
+                                        name="banner"
+                                        value={banner.higlighted}
+                                        checked={banner.higlighted}
+                                        onChange={(e) => changeHighlited(e, banner._id)}
+                                    />
+                                </td>
+                                <td className="px-4 py-2">
+                                    <img
+                                        className="h-6 w-6"
+                                        src="../Images/dashboardIcons/edit.png"
+                                        alt="edit"
+                                        onClick={() => openEditBannerModal(banner._id)}
+                                    />
                                 </td>
                             </tr>
-
                         ))}
                     </tbody>
                 </table>
@@ -72,12 +126,21 @@ function BannerSection() {
                 <div className="fixed inset-0 z-50 flex items-center justify-center">
                     <div className="fixed inset-0 bg-black opacity-50"></div>
                     <div className="bg-white p-6 relative z-10 w-96">
-                        <button onClick={closeEditBannerModal} className="absolute top-0 right-0 m-4 px-2 py-1">X</button>
-                        <EditBanner fetchBanners={fetchBanners} closeEditBannerModal={closeEditBannerModal} bannerID={selectedBannerID} banner={selectedBanner} />
+                        <button
+                            onClick={closeEditBannerModal}
+                            className="absolute top-0 right-0 m-4 px-2 py-1"
+                        >
+                            X
+                        </button>
+                        <EditBanner
+                            fetchBanners={fetchBanners}
+                            closeEditBannerModal={closeEditBannerModal}
+                            bannerID={selectedBannerID}
+                            banner={selectedBanner}
+                        />
                     </div>
                 </div>
             )}
-
         </div>
     );
 }
--- src/HomeComponents/HomeBanner.jsx
+++ src/HomeComponents/HomeBanner.jsx
@@ -1,21 +1,51 @@
+import { useState } from "react";
 import "../css/homebanner.css"
+import axios from "axios";
 
 function HomeBanner() {
-  return (
-    <div className="HomeBanner-container flex items-center justify-center">
-        <div className="HomeBanner-container-mini flex items-center justify-between my-16">
-    <div className="HomeBanner-title-btn ml-20">
-        <h1 className="text-4xl mb-4 w-96 italic">Check out our selection of Christmas gifts under 50$</h1>
-        <button className="bg-white text-red-700 font-bold py-2 px-10 border border-red-700  text-lg inline-block mt-5">
-            DISCOVER
-        </button>
-    </div>
-    <div>
-        <img src="../Images/merge.png" alt="" className="HomeBanner-img" />
-    </div>
-    </div>
-</div>
-  );
+    const [banner, setBanner] = useState("");
+
+    const goToLink = () => {
+        window.open(banner.link, '_blank');
+    };
+
+    const bannerToDisplay = async () => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_API_URL}/banner/getHiglighted`);
+            if (response) {
+                console.log("response", response.data.data[0]);
+                setBanner(response.data.data[0]);
+            }
+            else {
+                console.log("response", "empty");
+            }
+
+        } catch (error) {
+            console.error(`Error fetching banners' data: `, error);
+        }
+    };
+
+    bannerToDisplay();
+    console.log("banner", banner)
+
+    return (
+        <div className="HomeBanner-container flex items-center justify-center">
+            <div className="HomeBanner-container-mini flex items-center justify-between my-16">
+                <div className="HomeBanner-title-btn ml-20">
+                    <h1 className="text-4xl mb-4 w-96 italic">{banner.text}</h1>
+                    <button
+                        className="bg-white text-red-700 font-bold py-2 px-10 border border-red-700  text-lg inline-block mt-5"
+                        onClick={goToLink}
+                    >
+                        {banner.textButton}
+                    </button>
+                </div>
+                <div>
+                    <img src={banner.image} alt="" className="HomeBanner-img" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default HomeBanner;
