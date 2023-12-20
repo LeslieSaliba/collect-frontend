2d64ba4fada045719dc3f8887dfe86a990b3d652
AyaMouawi <ayamouawi8@gmail.com>
2023-12-20 13:38:20 +0200

    feat empty cart and wishlist illustrations

--- src/WishlistComponents/WishlistEmpty.jsx
+++ src/WishlistComponents/WishlistEmpty.jsx
@@ -5,6 +5,8 @@ function WishlistEmpty() {
     <div className="flex items-center justify-center h-screen">
       <div className="text-center mt-[-30%]">
       
+        
+        <img src='Images/empty-wishlist.png'></img>
         <p className="text-xl">Your wishlist is empty.</p>
        <Link to='/shop'> <button className="bg-white text-red-700 font-bold py-1 px-2 border border-red-700 w-32 text-lg inline-block mt-5 hover:bg-red-100">
           SHOP NOW
